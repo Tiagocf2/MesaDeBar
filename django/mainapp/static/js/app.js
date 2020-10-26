054b6eb75f7ffddc87418e0cf40648b87c15d306
Tiagocf2 <tiago.tcf2@gmail.com>
2020-10-26 14:04:51 -0300

    Resolvido bugs no APP
    Resolvido bug de inputs não poderem ter valor decimal em browsers que não suportam ',' no na tag input.

--- django/mainapp/static/js/app.js
+++ django/mainapp/static/js/app.js
@@ -6,8 +6,6 @@ function formatInput(self){
 	var new_value = self.value;
 	if(new_value == NaN){
 		new_value = 0;
-	} else {
-		new_value = new_value.replace(/[.]/g, ',');
 	}
 	new_value = parseFloat(new_value);
 	self.value = new_value.toFixed(2);
